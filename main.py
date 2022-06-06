392b8bddd06d067cc685a29b2c55324574e6c72c
jordanxlau <jordanxlau@yahoo.ca>
2022-06-06 17:25:43 -0400

    previous file was incorrectly uploaded so updated file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #chances = 'guesses'
-from random import randint
+import random
 import turtle
 
-num = randint(1,3)
+num = random.randint(1,3)
 
 chances = 6
 correct = 0
@@ -23,7 +23,7 @@ while chances > 0 and correct < 3:
     guess = input ('Guess a letter.')
     if guess == word[0] or guess == word[1] or guess == word[2]:
         print ('You correctly guessed the letter',guess,'!')
-        correct += 1
+        correct = correct + 1
         print ('You have',chances,'guesses left.')
         for i in range(3):
             if word[i] == guess:
@@ -34,32 +34,29 @@ while chances > 0 and correct < 3:
             print ('The word was ',wordList[num],'.')
     else:
         print ('Oh no! You guessed wrong!')
-        chances -= 1
+        chances = chances - 1
         print ('You have',chances,'guesses left.')
-        match (chances){
-            case 0:
-                j.backward(100)
-                j.left(90)
-                j.forward(100)
-                print ('Your man got hung :(. The correct word was.', wordList[num])
-            case 1:
-                j.backward(100)
-                j.right(55)
-                j.forward(200)
-                j.right(45)
-                j.forward(100)
-                
-            case 2:
-                j.backward(100)
-                j.left(110)
-                j.forward(100)
-            case 3:
-                j.backward(200)
-                j.right(55)
-                j.forward(100)
-            case 4:
-                j.right(90)
-                j.forward(250)
-            case 5:
-                j.circle(50)
-        }
+        if chances == 0:
+            j.backward(100)
+            j.left(90)
+            j.forward(100)
+            print ('Your man got hung :(. The correct word was.', wordList[num])
+        elif chances == 1:
+            j.backward(100)
+            j.right(55)
+            j.forward(200)
+            j.right(45)
+            j.forward(100)
+        elif chances == 2:
+            j.backward(100)
+            j.left(110)
+            j.forward(100)
+        elif chances == 3:
+            j.backward(200)
+            j.right(55)
+            j.forward(100)
+        elif chances == 4:
+            j.right(90)
+            j.forward(250)
+        elif chances == 5:
+            j.circle(50)
